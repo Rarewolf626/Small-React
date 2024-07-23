835c5fcc8886c1c6172c4a19c3b9a889f019add1
rare-wolf <gabeduncan018@gmail.com>
2024-07-23 17:22:09 +0300

    fix pizza presentational component styling

--- src/Components/Pizza/Pizza.js
+++ src/Components/Pizza/Pizza.js
@@ -1,8 +1,20 @@
+import './pizza.css';
+
 function Pizza(props) {
     return (
-        <div>
-            {props.pizza.name} 
-            <img src= {require(`../../../public/pizzas/${props.pizza.photoName}`)}/>
+        <div className="single_pizza">
+            <img className="pizza_img" src= {require(`../../../public/pizzas/${props.pizza.photoName}`)} alt={props.pizza.name}/>
+            <div className="pizza_info">
+                <div className="pizza_title">
+                    {props.pizza.name}
+                </div> 
+                <div className="pizza_subtitle">
+                    {props.pizza.ingredients}
+                </div> 
+                <div className="pizza_remaining">
+                    {props.pizza.soldOut ? 'Sold Out' : props.pizza.price}
+                </div> 
+            </div>  
         </div>
     );
 }
